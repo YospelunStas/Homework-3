74e97306f559748138f037bb899f78b2e8f789eb
Stanislav <stas2001.nikitin@yandex.ru>
2022-12-23 20:52:10 +0400

    Кубы чисел

---
+++ Task 23/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Cube (int x)
+{
+    int result = x * x * x;
+    return result;
+}
+
+Console.Write("Введите положительное число: ");
+string number = Console.ReadLine();
+int X = int.Parse(number);
+if (X >= 0)
+{
+    for (int i = 1; i <= (X); i++)
+    {
+        int num1 = Cube(i);
+        Console.WriteLine(num1);
+    }
+}
+else
+{
+    for (int i = -1; i >= (X); i--)
+    {
+        int num2 = Cube(i);
+        Console.WriteLine(num2);
+    }
+}
